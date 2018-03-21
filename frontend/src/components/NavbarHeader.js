a7613eba0dffcd57a9a4b2445f57aac881db1158
Haochen Li <hli2@illumina.com>
2018-03-20 17:32:42 -0700

    Add frontend user interface

---
+++ frontend/src/components/NavbarHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Navbar
+} from 'react-bootstrap';
+
+
+const NavbarHeader = () => {
+    return (
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">Readable</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+        </Navbar>
+    );
+};
+
+export default NavbarHeader;
+
